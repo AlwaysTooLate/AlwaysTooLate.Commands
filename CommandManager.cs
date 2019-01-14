be6fbda4ecff6de59ea1a45af54ee1ba21bb1c83
Damian Korczowski <erdroy2@gmail.com>
2019-01-14 02:35:52 +0100

    Some basic concepts

---
+++ CommandManager.cs
@@ -0,0 +1,42 @@
+﻿// AlwaysTooLate.Core (c) 2018-2019 Always Too Late. All rights reserved.
+
+using AlwaysTooLate.Core;
+
+namespace AlwaysTooLate.Commands
+{
+    /// <summary>
+    /// Command manager class.
+    /// Should be initialized on main (entry) scene.
+    /// </summary>
+    public class CommandManager : BehaviourSingleton<CommandManager>
+    {
+        protected override void OnAwake()
+        {
+            base.OnAwake();
+
+            // TODO: Register default commands (exec, print etc.)
+        }
+
+        public static void Execute(string command)
+        {
+            // Commands
+            // >exec somefile.txt
+            // >print "Hello, World!"
+            // >somefunction 2 2.0 "test" true false on off
+
+            // CVar integration
+            // >settings.cheats.fly true
+            // >settings.cheats.fly on
+            // >settings.cheats.fly 1
+            // >settings.cheats.fly
+            // on (default: off)
+
+            // TODO: Execute command
+        }
+
+        /*public static void Register(string name)
+        {
+        
+        }*/
+    }
+}
---
+++ CommandParser.cs
@@ -0,0 +1,29 @@
+﻿// AlwaysTooLate.Core (c) 2018-2019 Always Too Late. All rights reserved.
+
+using System.Collections.Generic;
+
+namespace AlwaysTooLate.Commands
+{
+    internal static class CommandParser
+    {
+        public static List<string> SplitCommand(string command)
+        {
+            return null;
+        }
+
+        public static bool ValidateCommand(List<string> command)
+        {
+            return false;
+        }
+
+        public static string GetCommandName(List<string> command)
+        {
+            return null;
+        }
+
+        public static List<string> GetCommandArguments(List<string> command)
+        {
+            return null;
+        }
+    }
+}
